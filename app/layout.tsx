1cf9d97d134f2b3f92553ea2e19c82843a4d0c53
tylerbry <tylergibbs049@gmail.com>
2023-12-22 15:30:25 -0600

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,20 +94,20 @@ export const metadata: Metadata = {
       { url: "/favicons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       {
-        url: "/favicons/android-icon-192x192.png",
+        url: "/favicons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
     ],
     shortcut: ["/favicons/favicon-32x32.png"],
     apple: [
-      { url: "/favicons/apple-icon.png" },
-      { url: "/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
+      { url: "/favicons/apple-touch-icon.png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
       {
-        rel: "apple-touch-icon-precomposed",
-        url: "/favicons/apple-icon-precomposed.png",
+        rel: "apple-touch-icon.png",
+        url: "apple-touch-icon.png",
       },
     ],
   },
