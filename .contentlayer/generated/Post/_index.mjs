73b6d732e546f8fe0b8dfa2acc7ce85e3a08cc9c
tylerbry <tylergibbs049@gmail.com>
2023-12-22 12:45:09 -0600

    added blog

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -2,6 +2,7 @@
 
 import posts__aiFaceSwapperMdx from './posts__ai-face-swapper.mdx.json' assert { type: 'json' }
 import posts__deployingNextAppsMdx from './posts__deploying-next-apps.mdx.json' assert { type: 'json' }
+import posts__mlxTutorialMdx from './posts__mlx-tutorial.mdx.json' assert { type: 'json' }
 import posts__ollamaTutorialMdx from './posts__ollama-tutorial.mdx.json' assert { type: 'json' }
 
-export const allPosts = [posts__aiFaceSwapperMdx, posts__deployingNextAppsMdx, posts__ollamaTutorialMdx]
+export const allPosts = [posts__aiFaceSwapperMdx, posts__deployingNextAppsMdx, posts__mlxTutorialMdx, posts__ollamaTutorialMdx]
